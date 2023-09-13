2f9640d2cc5f6828f038075db2e34535810b9db0
mohiandipta <mohianmostofa@gmail.com>
2023-09-13 12:59:04 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express ();
+app.use(express.json());
+const PORT = 4005
+
+app.get(`/`, (request, response) => {
+   const status = {
+      Status: "Simple node app (Running)"
+   }
+   
+   response.send(status);
+});
+
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
+});
